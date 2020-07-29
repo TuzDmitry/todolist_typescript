fc283625de86c90a49f30cd78c51af3e9ebc20fe
TuzDmitry <dimatuz94@gmail.com>
2020-07-29 18:41:09 +0300

    Updates

---
+++ precache-manifest.c764bbde588f07337cc64b4d97177add.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044869bf96dafee9555dd182c424d1d5",
+    "url": "/todolist_typescript/index.html"
+  },
+  {
+    "revision": "ffa2df8240aec11d1b0d",
+    "url": "/todolist_typescript/static/css/main.5c1f7d0f.chunk.css"
+  },
+  {
+    "revision": "93ca17b9baa3e2cd8fe9",
+    "url": "/todolist_typescript/static/js/2.86d60694.chunk.js"
+  },
+  {
+    "revision": "ffa2df8240aec11d1b0d",
+    "url": "/todolist_typescript/static/js/main.d564e482.chunk.js"
+  },
+  {
+    "revision": "5c8fb84513b414bed743",
+    "url": "/todolist_typescript/static/js/runtime~main.720423ee.js"
+  },
+  {
+    "revision": "ee01809c4573fdd8b1ab509986addc70",
+    "url": "/todolist_typescript/static/media/preloader.ee01809c.gif"
+  }
+]);
